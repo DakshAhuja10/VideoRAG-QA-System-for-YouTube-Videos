8968063c25c3fb1470f94bdceb7a5fa4656d76b1
DakshAhuja10 <148277234+DakshAhuja10@users.noreply.github.com>
2025-12-20 15:24:14 +0530

    commiting modifications

--- doc_loader.py
+++ doc_loader.py
@@ -1,6 +1,8 @@
 #here we create a langchain document object which store the text,metadata
 #this document object will then be used to create embeddings
-#here we do not need to split the text simply because we are generating the transcript line by line as can be seen in transcript_generate.py
+#here we do not need to split the text (We do not use a seperate text splitter)simply because we are generating the transcript line by line as can be seen in transcript_generate.py
+# each line contains transript sentence from video ,start and end duration so the text is already splitted  using a seperate text splitter can affect the start and end duration which may lead the model hallucinate
+
 import pandas as pd
 from hashlib import sha256
 from langchain.schema import Document
@@ -8,9 +10,10 @@ from langchain.schema import Document
 #we take the csv file as input and then compute the hash of the text column for every row 
 #this allows us to detect duplicate content and prevents us from generating their embedding repeatedly thereby save us cost by preventing us from making unnecessarily to google gemini api for embeddings 
 
-#this class returns a list of document object with page content as text and all the remaining columns as meta data
+#this class returns a list of document object with each document object contianing page content as text and all the remaining columns as meta data
 
 #if a text is repeated multiple times still we create a document object simply because start and end duration of those columns cannot be same
+
 
 class Csv_Loader:
     
